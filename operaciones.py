7ee2a1b62647ea1cd9f746887b26441751ff03cb
fatitaprogramando <nav24044@uvg.edu.gt>
2025-07-30 21:34:40 -0600

    agregue algunas perdon adrian

--- operaciones.py
+++ operaciones.py
@@ -8,3 +8,14 @@ class Conjunto:
 
     def agregar(self, elemento):
         self.elementos.add(elemento)
+
+    def remover(self, elemento):
+        self.elementos.remove(elemento)
+
+    def union(self, otro):
+        #es la operacion de A U B
+        nombre_resultado = f"({self.nombre} U {otro.nombre})"
+        elementos_resultado = self.elementos.union(otro.elementos)
+        return Conjunto(elementos_resultado, nombre_resultado)
+
+    #falta interseccion, diferencia, complemento etc...
